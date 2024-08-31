1db91ae3e0b8c14f8879f89b3378971efd9680af
Dep <109551168+dave-andrew@users.noreply.github.com>
2024-09-01 03:52:12 +0700

    feat: all menu pages, backsound

---
+++ Assets/BackgroundSound.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundSound : MonoBehaviour
+{
+    [SerializeField] private AudioClip backgroundSound;
+    void Awake()
+    {
+        SoundFXManager.instance.PlaySound(backgroundSound, transform, 1f);
+    }
+}
