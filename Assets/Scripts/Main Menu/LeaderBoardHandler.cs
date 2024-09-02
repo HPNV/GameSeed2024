9a2151cbff1378ec1298001744c9516a69c938cb
Dep <109551168+dave-andrew@users.noreply.github.com>
2024-09-03 00:58:54 +0700

    feat: left transition

--- Assets/Scripts/Main Menu/LeaderBoardHandler.cs
+++ Assets/Scripts/Main Menu/LeaderBoardHandler.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,10 +8,60 @@ public class LeaderBoardHandler : MonoBehaviour
 {
     public GameObject mainMenu;
     public GameObject leaderBoard;
+    
+    [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera;
+    [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera2;
 
     public void ToggleLeaderBoard()
     {
-        mainMenu.SetActive(!mainMenu.activeSelf);
-        leaderBoard.SetActive(!leaderBoard.activeSelf);
+        if (cinemachineVirtualCamera.Priority == 15)
+        {
+            cinemachineVirtualCamera.Priority = 20;
+            cinemachineVirtualCamera2.Priority = 15;
+        }
+        else
+        {
+            cinemachineVirtualCamera.Priority = 15;
+            cinemachineVirtualCamera2.Priority = 20;    
+        }
+        
+        // Fade in and out
+        if (mainMenu.activeSelf)
+        {
+            leaderBoard.SetActive(true);
+            StartCoroutine(FadeOut(mainMenu));
+            StartCoroutine(FadeIn(leaderBoard));
+        }
+        else
+        {
+            mainMenu.SetActive(true);
+            StartCoroutine(FadeOut(leaderBoard));
+            StartCoroutine(FadeIn(mainMenu));
+        }
+        
+        // mainMenu.SetActive(!mainMenu.activeSelf);
+        // leaderBoard.SetActive(!leaderBoard.activeSelf);
+    }
+    
+    private IEnumerator FadeOut(GameObject obj)
+    {
+        CanvasGroup canvasGroup = obj.GetComponent<CanvasGroup>();
+        while (canvasGroup.alpha > 0)
+        {
+            canvasGroup.alpha -= Time.deltaTime;
+            yield return null;
+        }
+        obj.SetActive(false);
+    }
+    
+    private IEnumerator FadeIn(GameObject obj)
+    {
+        CanvasGroup canvasGroup = obj.GetComponent<CanvasGroup>();
+        obj.SetActive(true);
+        while (canvasGroup.alpha < 1)
+        {
+            canvasGroup.alpha += Time.deltaTime;
+            yield return null;
+        }
     }
 }
