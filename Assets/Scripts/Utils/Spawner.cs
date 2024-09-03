2557654111d2c668cb7faa1087ba7b8fe78ac2c8
yahkerobertkertasnya <robert.wiliam12345@gmail.com>
2024-09-03 12:55:17 +0700

    feat: enemy spawn on new system

--- Assets/Scripts/Utils/Spawner.cs
+++ Assets/Scripts/Utils/Spawner.cs
@@ -43,9 +43,8 @@ public class Spawner : MonoBehaviour
         Vector3 spawnPosition = transform.position + Random.insideUnitSphere * spawnRadius;
         spawnPosition.y = Random.Range(spawnRadius * -1, spawnRadius);
         int enemyGacha = Random.Range(0, 100);
-
-        return;
-        if (enemyGacha < Int32.MaxValue)
+        
+        if (enemyGacha < 40)
         {
             enemyManager.Spawn(EnemyType.Ranged, spawnPosition);
         }
