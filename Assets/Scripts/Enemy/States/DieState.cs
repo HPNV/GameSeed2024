999eb38c3a5bcf9b3145d894a0efe1298b5d4af0
alexander ivan <sandhunter30@gmail.com>
2024-09-04 23:16:20 +0700

    Slime Death Sound

--- Assets/Scripts/Enemy/States/DieState.cs
+++ Assets/Scripts/Enemy/States/DieState.cs
@@ -10,9 +10,9 @@ namespace Enemy.States
         public override void OnEnter()
         {
             var position = Enemy.transform.position;
+            SoundFXManager.instance.PlayGameSoundOnce(Resources.Load<AudioClip>("Audio/Enemy/Slime Death"));
             SingletonGame.Instance.ExperienceManager.Spawn(3, new Vector3(position.x, position.y, position.z));
             SingletonGame.Instance.ResourceManager.Spawn(1, new Vector3(position.x, position.y, position.z));
-            
             Object.Destroy(Enemy.gameObject);
         }
     }
