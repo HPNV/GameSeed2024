05e75ac652a688334663d42091ba5b1aba0a7bbf
yahkerobertkertasnya <robert.wiliam12345@gmail.com>
2024-09-05 15:59:06 +0700

    feat: temp push for camera in new scene and catcharn attack

---
+++ Assets/Scripts/Plant/States/Cactharn/CactharnAttackState.cs
@@ -0,0 +1,19 @@
+﻿namespace Plant.States.Cactharn
+{
+    public class CactharnAttackState : PlantAttackState
+    {
+        public CactharnAttackState(Plant plant) : base(plant){}
+
+        public override void Update()
+        {
+            base.Update();
+            
+            var stateInfo = Plant.Animator.GetCurrentAnimatorStateInfo(0);
+            
+            if (stateInfo.IsName("Attack") && stateInfo.normalizedTime >= 1)
+            {
+                
+            }
+        }
+    }
+}
