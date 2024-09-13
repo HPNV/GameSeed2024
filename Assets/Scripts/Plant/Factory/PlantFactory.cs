d57ba5747b5bc1196f0d09582297e9ca9747d65e
H-A-L-F <arya.jayadi2013@gmail.com>
2024-09-13 11:01:36 +0700

    mapping achievement to plant

--- Assets/Scripts/Plant/Factory/PlantFactory.cs
+++ Assets/Scripts/Plant/Factory/PlantFactory.cs
@@ -17,16 +17,19 @@ namespace Plant.Factory
 
         [SerializeField] private List<EPlant> plants;
         [SerializeField] private List<PlantData> data;
+        [SerializeField] private List<EAchievement> eAchievements;
         [SerializeField] private PlantPlacementService plantPlacementService;
         private Dictionary<EPlant, PlantData> _plantsData;
         private Dictionary<EAchievement, EPlant> _achievementPlants;
 
         private void Start()
         {
-            _plantsData = new Dictionary<EPlant, PlantData>(); 
+            _plantsData = new Dictionary<EPlant, PlantData>();
+            _achievementPlants = new Dictionary<EAchievement, EPlant>();
             for (var i = 0; i < plants.Count; i++)
             {
                 _plantsData.Add(plants[i], data[i]);
+                _achievementPlants.Add(eAchievements[i], plants[i]);
             }
         }
 
