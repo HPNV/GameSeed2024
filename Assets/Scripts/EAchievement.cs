ee2bf5bc62a7f40c9fe66d2a9fd1aef9beefe2fb
H-A-L-F <arya.jayadi2013@gmail.com>
2024-09-13 09:34:17 +0700

    enum achievements

---
+++ Assets/Scripts/EAchievement.cs
@@ -0,0 +1,51 @@
+﻿namespace Script
+{
+    public enum EAchievement
+    {
+        // Planting Seeds
+        NewGardener,            // Plant 2 plants
+        BloomBooster,           // Plant 20 plants
+        GardeningGuru,          // Plant 50 plants
+        PerfectPlanter,         // Plant 100 plants
+        PlantInvasion,          // Plant 1000 plants
+        MasterGardener,         // Plant 5000 plants
+        SpeedPlanter,           // Plant 10 plants in 1 minute
+        FrenziedFarmer,         // Plant 50 plants in under 5 minutes
+        PlantCommander,         // Have 50 plants on the field at once
+        PlantHoarder,           // Have 100 plants on the field at once
+
+        // Leveling Up
+        QuickLearner,           // Level up 3 times in under 1 minute
+        PlantPotential,         // Level up 5 times
+        LevelUpEnthusiast,      // Level up 20 times
+        LevelUpVeteran,         // Level up 50 times
+
+        // Upgrade Plant
+        UpgradeApprentice,      // Upgrade at least 3 plants
+        UpgradeMaster,          // Upgrade at least 10 plants
+        UpgradeOverachiever,    // Upgrade at least 30 plants
+        FullyBloomed,           // Fully upgrade any plant
+        GardenersGlory,         // Achieve 10 upgraded plants
+
+        // Kill Enemy
+        FirstBlood,             // Kill your first enemy
+        KillerSeed,             // Kill 10 enemies
+        EfficientKiller,        // Kill 50 enemies in 5 minutes
+        MonsterSlayer,          // Kill 500 enemies
+        UnstoppableForce,       // Defeat 1000 enemies in total
+        EndlessOnslaught,       // Kill 5000 enemies in total
+        MonsterFrenzy,          // Kill 200 enemies in under 10 minutes
+        DoomsdayGardener,       // Kill 200 enemies using explosive plants
+        TrapSpecialist,         // Kill 50 enemies with Boomkin
+
+        // Explode Enemy
+        ExplosiveExpertise,     // Explode 5 enemies
+
+        // Resource Collection
+        ResourceCollector,      // Collect 15 resources
+        ResourceHoarder,        // Collect 30 resources
+        HerbalHarvester,        // Collect 50 resources
+        ResourceTycoon,         // Collect 1000 resources
+        ResourcefulMind         // Collect 5000 resources in total
+    }
+}
