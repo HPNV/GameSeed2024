78acee62f1a22b4894731b0858b7b44bfe78ff56
Dep <109551168+dave-andrew@users.noreply.github.com>
2024-09-16 00:43:48 +0700

    fix: cutscene bug

--- Assets/ExitSceneHandler.cs
+++ Assets/ExitSceneHandler.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Firebase.Firestore;
+using Firebase.Extensions;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -11,12 +13,59 @@ public class ExitSceneHandler : MonoBehaviour
     public Image LoadingBarFill;
     public int sceneId;
 
+    private FirebaseFirestore db;
+    
     public void PlayGame(int sceneId)
     {
-        Console.WriteLine("Loading scene " + sceneId);
+        
+        db = FirebaseFirestore.DefaultInstance;
+        
+        HighScoreUpdate();
+        
         this.sceneId = sceneId;
 
         StartCoroutine(LoadScene(sceneId));
+    }
+    
+    private void HighScoreUpdate()
+    {
+        string hostname = System.Environment.MachineName;
+        db = FirebaseFirestore.DefaultInstance;
+        DocumentReference docRef = db.Collection("users").Document(hostname);
+        
+        // get the user highest_score then compare it to the current score
+        docRef.GetSnapshotAsync().ContinueWithOnMainThread(task =>
+        {
+            if (task.IsCompleted)
+            {
+                DocumentSnapshot snapshot = task.Result;
+                if (snapshot.Exists)
+                {
+                    Dictionary<string, object> data = snapshot.ToDictionary();
+                    int highest_score = (int) data["highest_score"];
+                    if (SingletonGame.Instance.homeBase.score > highest_score)
+                    {
+                        UpdateHighScore(docRef);
+                    }
+                }
+            }
+        });
+    }
+
+    private void UpdateHighScore(DocumentReference docRef)
+    {
+        
+        Dictionary<string, object> data = new Dictionary<string, object>
+        {
+            {"highest_score", SingletonGame.Instance.homeBase.score}
+        };
+        docRef.SetAsync(data).ContinueWithOnMainThread(task =>
+        {
+            if (task.IsCompleted)
+            {
+                Debug.Log("High Score Updated");
+            }
+        });
     }
 
     IEnumerator LoadScene(int sceneId)
